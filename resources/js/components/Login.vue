ae49f4e2d07f8c94aa8bdb2aaa430664c74e8ee9
hector-alcaide <halcaide6@gmail.com>
2023-04-12 16:22:27 +0200

    guardar

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,67 +1,28 @@
 <template>
-<!--    <div class="auth-div">-->
-<!--        <div class="auth-background" id="login-div">-->
-<!--            <div class="row justify-content-center">-->
-<!--                <div class="col-md-8">-->
-<!--                    <div v-if="error !== null" class="alert alert-danger alert-dismissible fade show" role="alert">-->
-<!--                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>-->
-
-
-<!--                        <strong>{{error}}</strong>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="auth-form">-->
-<!--                <form>-->
-<!--                    <div class="form-group row">-->
-<!--                        <div class="col-md-12">-->
-<!--                            <input id="email" type="email" class="form-control" v-model="email" required autofocus autocomplete="off" placeholder="Email">-->
-<!--                        </div>-->
-<!--                    </div>-->
-
-<!--                    <div class="form-group row mt-3">-->
-<!--                        <div class="col-md-12">-->
-<!--                            <input id="password" type="password" class="form-control" v-model="password" required autocomplete="off" placeholder="Contraseña">-->
-<!--                        </div>-->
-<!--                    </div>-->
-
-<!--                    <div class="form-group row mt-3">-->
-<!--                        <div class="col-md-8">-->
-<!--                            <button type="submit" class="button_form d-block mx-auto" @click="doLogin">-->
-<!--                                Iniciar sesión-->
-<!--                            </button>-->
-<!--                        </div>-->
-<!--                        <div class="col-md-4">-->
-<!--                            <router-link to="/register" >Register</router-link>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </form>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-
     <div class="auth-div">
         <div class="auth-background" id="login-div">
             <div class="auth-formdiv">
                 <form class="auth-form">
                     <div class="row ps-5 pe-4 pt-2 mb-3">
-                        <div class="col-md-12 ps-5">
+                        <div class="col-md-12 ps-5 pe-3">
                             <input id="alias" type="alias" class="form-control" v-model="alias" required autofocus autocomplete="off" placeholder="Alias">
                         </div>
                     </div>
                     <div class="row ps-5 pe-4 pt-2 mb-3">
-                        <div class="col-md-12 pt-1 ps-5">
+                        <div class="col-md-12 pt-1 ps-5 pe-3">
                             <input id="password" type="password" class="form-control" v-model="password" required autocomplete="off" placeholder="Contraseña">
                         </div>
                     </div>
-                    <div class="row mt-3">
+                    <div class="row mt-3 pe-4">
                         <div class="col-md-8">
                             <button type="submit" class="button_form mx-auto" @click="doLogin">
                                 Iniciar sesión
                             </button>
                         </div>
-                        <div class="col-md-4">
-                            <router-link to="/register" >Registrar se</router-link>
+                        <div class="col-md-4 ps-1">
+                            <button type="button" class="button_form btn_form_secondary button_secondary" @click="$router.push('/register')">
+                                Registrar se
+                            </button>
                         </div>
                     </div>
                 </form>
