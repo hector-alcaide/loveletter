d1e93741b5fc04bae68e4d2363cb6b097802ed03
jacob <jacobbarbefp@ibf.cat>
2023-04-20 17:50:09 +0200

    amigos1.2

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,6 +3,7 @@ import {createWebHistory, createRouter} from "vue-router";
 import Home from '../components/Home.vue';
 import Register from '../components/Register.vue';
 import Login from '../components/Login.vue';
+import Amigos from '../components/Amigos.vue';
 
 export const routes = [
     {
@@ -22,6 +23,11 @@ export const routes = [
         name: 'login',
         path: '/login',
         component: Login
+    },
+    {
+        name: 'amigos',
+        path: '/amigos',
+        component: Amigos
     },
     {
         path: '/:pathMatch(.*)*',
