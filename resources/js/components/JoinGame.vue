1d54439ebc463c1029c061731f788ba2b2813c6c
hector-alcaide <halcaide6@gmail.com>
2023-05-03 20:15:49 +0200

    guarda

--- resources/js/components/JoinGame.vue
+++ resources/js/components/JoinGame.vue
@@ -1,4 +1,35 @@
 <template>
+    <div class="text-center mt-lg-3">
+        <a href="Home.vue"><img class="logo" src="../../images/logo.png"></a>
+    </div>
+    <div class="divReturn">
+        <button class="d-block return buttonClose" @click="$router.push('/home')">Volver</button>
+    </div>
+    <div class="modal fade" id="modalFriends" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Amigos</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input type="text">
+                        </div>
+                        <div class="col-md-6">
+                            <button v-if="this.game && this.game.idHost == this.idUser" type="button" class="mx-auto" @click="prepareGame(this.idGame)">Buscar</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div v-if="this.loadingData" class="pantalla-carga">
         <span><p>Cargando...</p></span>
     </div>
@@ -14,9 +45,9 @@
 <!--    <p>Partida numero {{ idGame }}</p>-->
 <!--    <p>Anfitrion {{ idHost }}</p>-->
 
+    <button v-if="this.game && this.game.idHost == this.idUser" type="button" class="mx-auto" @click="prepareGame(this.idGame)">Empezar game</button>
 
-
-    <button v-if="this.game && this.game.idHost == this.idUser" class="mx-auto" @click="prepareGame(this.idGame)">Empezar game</button>
+    <button type="button" class="mx-auto button_secondary" data-bs-toggle="modal" data-bs-target="#modalFriends" @click="listFriends()">Invitar</button>
 
     <!--    <div v-for="item in items" :key="item.id">-->
 <!--        {{ item.name }}-->
@@ -35,6 +66,7 @@ export default {
             idUser: window.Laravel.user.idUser,
             game: null,
             users: [],
+            friends: [],
             echo: new Echo({
                 broadcaster: 'pusher',
                 key: 'local',
@@ -98,6 +130,12 @@ export default {
                 console.log(response)
             });
         },
+        listFriends(){
+            this.$axios.get('/api/getfriends').then(response => {
+                console.log(response.data)
+                this.friends = response.data;
+            });
+        }
     }
 }
 </script>
