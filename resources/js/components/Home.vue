ff045c4fe9da182c52a78d38b234f581a93c84f5
hector-alcaide <halcaide6@gmail.com>
2023-05-08 19:25:43 +0200

    guardar

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="bg-image1">    
-        <div class="text-center mt-lg-2">
-            <img class="logo" src="../../images/logo.png">
-        </div>
-        <div class="text-center mt-lg-4">
-            <div class="d-inline-block me-lg-5" style="padding-bottom: 8rem;">
-                <img class="guardia" src="../../images/left_guard.png">
+    <div class="container">
+        <div class="bg-image1">
+            <div class="text-center">
+                <img class="logo" src="../../images/logo.png">
             </div>
-            <div class="d-inline-block align-middle text-center mt-lg-3 pb-lg-5 mx-lg-5">
-                <button class="button_jugar d-block mx-auto mt-lg-5" @click="$router.push('/games')">Jugar</button>
-                <button class="button_menu d-block mx-auto mt-lg-5 mb -lg-4" @click="$router.push('/profile')">Perfil</button>
-                <button class="button_menu d-block mx-auto my-lg-4" @click="$router.push('/friends')">Amigos</button>
-                <button class="button_menu d-block mx-auto my-lg-4" @click="$router.push('/board')">Ranking</button>
+            <div class="text-center mt-lg-4">
+                <div class="d-inline-block me-lg-5" style="padding-bottom: 8rem;">
+                    <img class="guardia" src="../../images/left_guard.png">
+                </div>
+                <div class="d-inline-block align-middle text-center mt-lg-3 pb-lg-5 mx-lg-5">
+                    <button class="button_jugar d-block mx-auto mt-lg-5" @click="$router.push('/games')">Jugar</button>
+                    <button class="button_menu d-block mx-auto mt-lg-5 mb -lg-4" @click="$router.push('/profile')">Perfil</button>
+                    <button class="button_menu d-block mx-auto my-lg-4" @click="$router.push('/friends')">Amigos</button>
+                    <button class="button_menu d-block mx-auto my-lg-4" @click="$router.push('/board')">Ranking</button>
+                </div>
+                <div class="d-inline-block ms-lg-5">
+                    <img class="guardia" src="../../images/right_guard.png">
+                </div>
             </div>
-            <div class="d-inline-block ms-lg-5">
-                <img class="guardia" src="../../images/right_guard.png">
-            </div>
-            <div class="shield_yes"></div>
         </div>
     </div>
-
-
 </template>
 
 <script>
